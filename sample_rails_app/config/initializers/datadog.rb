3c02652a47c3eaa61382f87d62792b9f1b8390b9
shinter61 <shintaro-matsumoto@c-fo.com>
2023-07-11 11:47:40 +0900

    Install dd-trace

---
+++ sample_rails_app/config/initializers/datadog.rb
@@ -0,0 +1,7 @@
+Datadog.configure do |c|
+  # Configure the Datadog tracer here.
+  # Activate integrations, change tracer settings, etc...
+  # By default without additional configuration,
+  # no additional integrations will be traced, only
+  # what you have instrumented with OpenTracing.
+end

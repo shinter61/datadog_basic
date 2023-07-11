8925920192782510d7cdaa111bc718a36961a7c4
shinter61 <shintaro-matsumoto@c-fo.com>
2023-07-11 12:05:00 +0900

    Add manually tracing

--- sample_rails_app/app/controllers/application_controller.rb
+++ sample_rails_app/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def index
+    Datadog::Tracing.trace('index', service: 'sre-onboarding-shinta', resource: 'GET /') do |span|
+      render status: :ok, json: { body: 'Hello, OKuRa!' }
+    end
+  end
 end
